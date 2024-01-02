f28a6e3896ff0695e9d245c3b478740249536647
Saarujan <sy2g19@soton.ac.uk>
2024-01-02 01:50:54 +0000

    display_analytics is copied from cwk1 get, submit_game is copied from cwk1 prompt_create

--- display_analytics/__init__.py
+++ display_analytics/__init__.py
@@ -2,41 +2,38 @@ import json
 import logging
 import azure.functions as func
 from azure.cosmos import CosmosClient
-from azure.cosmos.exceptions import CosmosHttpResponseError
 import os
 
-MyCosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
-PlayerDBProxy = MyCosmos.get_database_client(os.environ['Database'])
-PlayerContainerProxy = PlayerDBProxy.get_container_client(os.environ['PlayerContainer'])
+# Initialize Cosmos DB client
+cosmos_client = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+player_db_proxy = cosmos_client.get_database_client(os.environ['DatabaseName'])
+player_container_proxy = player_db_proxy.get_container_client(os.environ['PlayerContainer'])
+prompt_container_proxy = player_db_proxy.get_container_client(os.environ['PromptContainer'])
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Registering player')
-
-    player = req.get_json()
+    logging.info('Processing get prompts request')
     
-    if (len(player["username"]) < 4 or len(player["username"]) > 14):
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Username less than 4 characters or more than 14 characters"}))
-    elif (len(player["password"]) < 10 or len(player["password"]) > 20):
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Password less than 10 characters or more than 20 characters"}))
-    
-    query = "SELECT * FROM player WHERE player.username = @username"
-    parameters = [{"name": "@username", "value": player["username"]}]
-    items = list(PlayerContainerProxy.query_items(
-        query=query,
-        parameters=parameters,
-        enable_cross_partition_query=True
-    ))
-
-    if items:
-        # Check username already exists
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Username already exists"}))
-
     try:
-        player['games_played'] = 0
-        player['total_score'] = 0
-        PlayerContainerProxy.create_item(body=player, enable_automatic_id_generation=True)
-        body_json = json.dumps({"result": True, "msg": "OK"})
-        return func.HttpResponse(body=body_json)
+        request_data = req.get_json()
+        players = request_data.get('players', [])
+        language = request_data.get('language', '')
 
-    except CosmosHttpResponseError as e:
+        if not players:
+            return func.HttpResponse(json.dumps([])) # Returns empty list if no players provided
+
+        # Construct the query to handle multiple players
+        query = "SELECT c.id, c.username, t.text FROM c JOIN t IN c.texts WHERE ARRAY_CONTAINS(@players, c.username) AND t.language = @language"
+        parameters = [{"name": "@players", "value": players}, {"name": "@language", "value": language}]
+        
+        # Fetch prompts
+        prompts = list(prompt_container_proxy.query_items(query=query, parameters=parameters, enable_cross_partition_query=True))
+
+        # Format the response
+        formatted_prompts = []
+        for prompt in prompts:
+            formatted_prompts.append({"id": prompt['id'], "text": prompt['text'], "username": prompt['username']})
+
+        return func.HttpResponse(json.dumps(formatted_prompts, ensure_ascii=False))
+
+    except Exception as e:
         return
--- submit_game/__init__.py
+++ submit_game/__init__.py
@@ -1,42 +1,121 @@
 import json
 import logging
 import azure.functions as func
-from azure.cosmos import CosmosClient
-from azure.cosmos.exceptions import CosmosHttpResponseError
+from azure.cosmos import CosmosClient, exceptions
+import requests
+import uuid
 import os
 
-MyCosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
-PlayerDBProxy = MyCosmos.get_database_client(os.environ['Database'])
+# Initialize Cosmos DB client
+cosmos_client = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+PlayerDBProxy = cosmos_client.get_database_client(os.environ['Database'])
 PlayerContainerProxy = PlayerDBProxy.get_container_client(os.environ['PlayerContainer'])
+PromptContainerProxy = PlayerDBProxy.get_container_client(os.environ['PromptContainer'])
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Registering player')
-
-    player = req.get_json()
-    
-    if (len(player["username"]) < 4 or len(player["username"]) > 14):
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Username less than 4 characters or more than 14 characters"}))
-    elif (len(player["password"]) < 10 or len(player["password"]) > 20):
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Password less than 10 characters or more than 20 characters"}))
-    
-    query = "SELECT * FROM player WHERE player.username = @username"
-    parameters = [{"name": "@username", "value": player["username"]}]
-    items = list(PlayerContainerProxy.query_items(
-        query=query,
-        parameters=parameters,
-        enable_cross_partition_query=True
-    ))
-
-    if items:
-        # Check username already exists
-        return func.HttpResponse(body=json.dumps({"result": False, "msg": "Username already exists"}))
+    logging.info('Processing prompt creation request')
 
     try:
-        player['games_played'] = 0
-        player['total_score'] = 0
-        PlayerContainerProxy.create_item(body=player, enable_automatic_id_generation=True)
-        body_json = json.dumps({"result": True, "msg": "OK"})
-        return func.HttpResponse(body=body_json)
+        request_data = req.get_json()
 
-    except CosmosHttpResponseError as e:
+        # Validate player existence
+        if not is_player_exists(request_data['username']):
+            return func.HttpResponse(json.dumps({"result": False, "msg": "Player does not exist"}))
+
+        # Validate prompt length
+        prompt_text = request_data['text']
+        if len(request_data['text']) < 15 or len(prompt_text) > 80:
+            return func.HttpResponse(json.dumps({"result": False, "msg": "Prompt less than 15 characters or more than 80 characters"}))
+        
+        # Detect language
+        detected_languages = detect_language(prompt_text)
+        primary_language = detected_languages[0]  # First reslut = main language
+        if primary_language['language'] not in ['en', 'es', 'it', 'sv', 'ru', 'id', 'bg', 'zh-Hans']:
+            return func.HttpResponse(json.dumps({"result": False, "msg": "Unsupported language"}))
+        if primary_language['score'] < 0.3:
+            return func.HttpResponse(json.dumps({"result": False, "msg": "Unsupported language"}))
+
+        # Translate text
+        translated_texts = translate_text(prompt_text, primary_language['language'])
+
+        # Insert prompt into Cosmos DB
+        create_prompt(request_data['username'], request_data['text'], translated_texts, primary_language['language'])
+        return func.HttpResponse(json.dumps({"result": True, "msg": "OK"}))
+
+    except Exception as e:
+        return {"result": False, "msg": "BLA"}
+
+# Function to check if player exists
+def is_player_exists(username):
+    query = "SELECT * FROM player WHERE player.username = @username"
+    parameters = [{"name": "@username", "value": username}]
+    items = list(PlayerContainerProxy.query_items(query=query, parameters=parameters, enable_cross_partition_query=True))
+    return len(items) > 0
+
+def detect_language(text):
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+    path = '/detect'
+    constructed_url = endpoint + path
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': "0144b604f2354054aef9dbaa784db5d6",
+        'Ocp-Apim-Subscription-Region': "uksouth",
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    params = {
+        'api-version': '3.0',
+    }
+
+    body = [{'text': text}]
+
+    response = requests.post(constructed_url, params=params, headers=headers, json=body)
+    return response.json()
+
+# Function to translate text
+def translate_text(text, from_lang):
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    # List of target languages for translation
+    target_languages = ['en', 'es', 'it', 'sv', 'ru', 'id', 'bg', 'zh-Hans']
+    target_languages.remove(from_lang)  # Remove the source language from the list
+
+    params = {
+        'api-version': '3.0',
+        'from': from_lang,
+        'to': target_languages
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': "0144b604f2354054aef9dbaa784db5d6",
+        'Ocp-Apim-Subscription-Region': "uksouth",
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{'text': text}]
+
+    response = requests.post(constructed_url, params=params, headers=headers, json=body)
+    return response.json()
+
+
+def create_prompt(username, original_text, translated_texts, original_language):
+    # Initialize with original text
+    texts = [{"language": original_language, "text": original_text}]
+
+    # Add translations, including English if necessary
+    for translation in translated_texts:
+        for text in translation['translations']:
+            if text["to"] != original_language or (original_language != 'en' and text["to"] == 'en'):
+                texts.append({"language": text["to"], "text": text["text"]})
+
+    prompt_item = {"username": username, "texts": texts}
+
+    try:
+        # Insert into container
+        PromptContainerProxy.create_item(prompt_item, enable_automatic_id_generation=True)
+    except Exception as e:
         return
