9420ff0ca0ec6577fbdbe31b438d26df2a7e4841
Saarujan <sy2g19@soton.ac.uk>
2024-01-04 06:29:04 +0000

    requirements.txt

--- tests/test_submit_game.py
+++ tests/test_submit_game.py
@@ -3,7 +3,7 @@ import json
 import requests
 from azure.cosmos import CosmosClient
 
-class test_prompt_create(unittest.TestCase):
+class test_submit_game(unittest.TestCase):
 
     LOCAL_DEV_URL="http://localhost:7071/"
     PUBLIC_URL="https://gamecraft-backend.azurewebsites.net/submit_game?code=cPs-op-r7rAFOF_eKXg08jmTiwjRyN1s6gA_dOBKfp5lAzFuM4CODQ=="
@@ -11,9 +11,8 @@ class test_prompt_create(unittest.TestCase):
 
     def test_player_register(self):
         
-        payload = {'name': 'Luis', 'devName': 'UbiSoft', 'devName': 'El programa unifilar de Python más inútil'
-                   ,'description': 'shooter', 'image': 'n/a'
-                   ,'options': 'fire, down, up', 'sharePrice': '$2', 
+        payload = {'name': 'Luis', 'devName': 'UbiSoft', 'description': 'shooter', 'image': 'n/a'
+                   ,'options': 'fire, down, up', 'roadmap': 'roadmap', 'sharePrice': '$2', 
                    'minThreshold': '10 votes', 'revenueSharing': '$5'
                    , }
         json_payload = json.dumps(payload)
